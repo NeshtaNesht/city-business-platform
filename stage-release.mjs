ab4d129f8a060b77ab7be361ca2f36a1a1fe72e6
Murashov Anton <dante.pb.b@gmail.com>
2023-05-11 11:32:25 +0500

    stage release command

---
+++ stage-release.mjs
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+import { readFileSync, writeFileSync } from "fs";
+import { execSync } from "child_process";
+
+const version =
+  process.argv[process.argv.length - 1].match(/^\d.\d.\d$/gm)?.[0];
+
+const fileName = "package.json";
+
+if (!version) {
+  throw new Error("Version not provided");
+}
+
+const file = JSON.parse(readFileSync(fileName, "utf8"));
+
+if (!file) {
+  throw new Error("package.json not found");
+}
+
+file.version = version;
+writeFileSync(fileName, JSON.stringify(file, null, 2));
+console.log("Версия указана");
+console.log("Собираю проект...");
+try {
+  execSync("npm run build");
+} catch (e) {
+  console.log("error", e);
+}
+execSync('move "./build" "./docs"');
+console.log("Сборка готова");
+process.exit(0);
